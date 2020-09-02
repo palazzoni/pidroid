c4787ddbbffc83304dc700a9dc9be764958ef954
Michele Palazzoni <43499624+palazzoni@users.noreply.github.com>
2020-09-02 17:39:53 +0200

    Add files via upload

---
+++ android/blackrestore.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+/sbin/.magisk/busybox/busybox tar -xvpzf /sdcard/suse_backup.tar.gz -C /data/local/
+exit 0
+
---
+++ android/blacktar.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+/sbin/.magisk/busybox/busybox tar -cvpz -f /sdcard/suse_backup.tar.gz suse
+exit 0
